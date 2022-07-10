5309c74eb856fca4b40c463100027db0e5e3870c
Luca Cucchetti <luca.f.cucchetti@gmail.com>
2022-07-10 23:21:21 +0100

    ERD and minor fixes

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -20,7 +20,7 @@ export class ApiStack extends Stack {
     });
 
     // Default endpoint type is EndpointType.EDGE which suits us fine: https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-api-endpoint-types.html
-    const api = new LambdaRestApi(this, 'DuckAwareness-API', {
+    new LambdaRestApi(this, 'DuckAwareness-API', {
       endpointExportName: 'DuckAwareness-APIUrl',
       proxy: true, // route all requests to the Lambda Function, in the future if we want to use the same API for the UI we might want to add a proxy resouce instead
       handler,
