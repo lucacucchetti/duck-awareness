047e093ab54bf94c27d143607a55469892c9918a
Mikhail Domanoski <332960+mikhasd@users.noreply.github.com>
2022-07-12 10:57:00 +0100

    Added deploy job to CI

--- bin/duck-awareness.ts
+++ bin/duck-awareness.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { ApiStack } from '../lib/api-stack';
+import { AwsGithubOidc } from '../lib/aws-github-oidc';
 
 const app = new cdk.App();
 new ApiStack(app, 'DuckAwareness-APIStack');
+new AwsGithubOidc(app, 'AwsGithubOidc')
--- lib/aws-github-oidc.ts
+++ lib/aws-github-oidc.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps, } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput} from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { PrincipalWithConditions } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
@@ -7,10 +7,10 @@ export class AwsGithubOidc extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const ghOidcProvider = new iam.OpenIdConnectProvider(this, 'github-oidc-provider', {
+        const ghOidcProvider = new iam.OpenIdConnectProvider(this, 'github-oidc-provider', {            
             url: 'https://token.actions.githubusercontent.com',
             thumbprints: ['6938fd4d98bab03faadb97b34396831e3780aea1'],
-            clientIds: ['https://github.com/lucacucchetti']
+            clientIds: ['https://github.com/lucacucchetti','sts.amazonaws.com']
         })
 
         const ghFederatedPrincipal = new iam.FederatedPrincipal(ghOidcProvider.openIdConnectProviderArn, {
@@ -20,11 +20,18 @@ export class AwsGithubOidc extends Stack {
         })
 
         const ghRole = new iam.Role(this, 'github-actions', {
+            description: 'used by Github Actions to deploy the application stack',
             roleName: 'github-actions',
             assumedBy: ghFederatedPrincipal,
             managedPolicies: [
                 iam.ManagedPolicy.fromManagedPolicyArn(this, 'github-actions-read-only-access' ,'arn:aws:iam::aws:policy/ReadOnlyAccess')
-            ]
+            ],
         })
+
+        const h = new CfnOutput(this, 'githubActionsRoleArn', {
+            description: 'Github Actions role ARN',
+            value: ghRole.roleArn
+        })
+
     }
 }
