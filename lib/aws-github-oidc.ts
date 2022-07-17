016ccffe1cdf85b84b13fa86a9f779d633ee4822
mikhasd <332960+mikhasd@users.noreply.github.com>
2022-07-17 20:43:39 +0100

    correct pipeline branch name and adjusted stack imports

--- lib/aws-github-oidc.ts
+++ lib/aws-github-oidc.ts
@@ -1,36 +1,43 @@
-import { Stack, StackProps, CfnOutput} from 'aws-cdk-lib';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { PrincipalWithConditions } from 'aws-cdk-lib/aws-iam';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import {
+    Effect,
+    FederatedPrincipal,
+    ManagedPolicy,
+    OpenIdConnectProvider,
+    PolicyDocument,
+    PolicyStatement,
+    Role
+} from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class AwsGithubOidc extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
 
-        const ghOidcProvider = new iam.OpenIdConnectProvider(this, 'github-oidc-provider', {            
+        const ghOidcProvider = new OpenIdConnectProvider(this, 'github-oidc-provider', {
             url: 'https://token.actions.githubusercontent.com',
             thumbprints: ['6938fd4d98bab03faadb97b34396831e3780aea1'],
-            clientIds: ['https://github.com/lucacucchetti','sts.amazonaws.com']
+            clientIds: ['sts.amazonaws.com']
         })
 
-        const ghFederatedPrincipal = new iam.FederatedPrincipal(ghOidcProvider.openIdConnectProviderArn, {            
+        const ghFederatedPrincipal = new FederatedPrincipal(ghOidcProvider.openIdConnectProviderArn, {
             'StringLike': {
                 'token.actions.githubusercontent.com:sub': 'repo:lucacucchetti/duck-awareness:*'
             }
         }, 'sts:AssumeRoleWithWebIdentity')
 
-        const ghRole = new iam.Role(this, 'github-actions', {
+        const ghRole = new Role(this, 'github-actions', {
             description: 'used by Github Actions to deploy the application stack',
             roleName: 'github-actions',
             assumedBy: ghFederatedPrincipal,
             managedPolicies: [
-                iam.ManagedPolicy.fromManagedPolicyArn(this, 'github-actions-read-only-access' ,'arn:aws:iam::aws:policy/ReadOnlyAccess')
+                ManagedPolicy.fromManagedPolicyArn(this, 'github-actions-read-only-access', 'arn:aws:iam::aws:policy/ReadOnlyAccess')
             ],
             inlinePolicies: {
-                AllowAssumeCdkRole: new iam.PolicyDocument({
-                    statements: [new iam.PolicyStatement({
+                AllowAssumeCdkRole: new PolicyDocument({
+                    statements: [new PolicyStatement({
                         actions: ['sts:AssumeRole'],
-                        effect: iam.Effect.ALLOW,
+                        effect: Effect.ALLOW,
                         resources: [`arn:aws:iam::${this.account}:role/cdk*`]
                     })]
                 })
