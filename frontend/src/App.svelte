b44784f86ccecf67417bc949cc60bdbae414d2f9
Mikhail Domanoski <332960+mikhasd@users.noreply.github.com>
2022-08-24 22:57:41 +0100

    Simple Notes UI

---
+++ frontend/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import {
+  Content,Header,SkipToContent
+  } from "carbon-components-svelte";
+  import "carbon-components-svelte/css/white.css";
+  import NoteList from "./lib/NoteList.svelte";
+
+</script>
+
+<Header company="Duck" platformName="Awareness">
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+</Header>
+
+<Content>
+  <NoteList />
+</Content>
